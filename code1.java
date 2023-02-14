8e5934122ff6a332de44ec61ca45ef5f1cde9c6d
ViveksRajput9 <vivekraj60271@gmail.com>
2023-02-14 19:17:54 +0530

    first commit

---
+++ code1.java
@@ -0,0 +1,7 @@
+public class code1{
+  public static void main(String args []){
+         
+      System.out.println("hello world"); 
+      System.out.println("my name is vivek singh rajput")
+ }
+}
---
+++ palamdronicPattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class palandromicPattern{
+  public static void main(String args[]){
+
+   Scanner sc= new scanner(System.in); 
+   int n=sc.nextInt();
+   int a,b,c;
+   for( a = 1;a<=n;a++){
+   for( b = 1;b<=n-a;b++){
+   System.out.print(" ");
+   }
+   for(b = a;b>0;b--){
+   System.out.print(b)
+   }
+   for(b=2;b<=a;b++){
+   System.out.print("b")
+    }
+   }
+   System.out.println();
+ }
+}
